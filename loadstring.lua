37333829800fe3b6eaa63b7e3eb8905efb2ab6bd
EnterpriseExperience <165463731+EnterpriseExperience@users.noreply.github.com>
2024-11-04 03:05:22 -0500

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EnterpriseExperience/ParadisRPScript/refs/heads/main/source'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EnterpriseExperience/ParadiseRPScript/refs/heads/main/source'))()
